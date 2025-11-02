fd14ae82d0fe574c00d513d18615fdd227071dd6
Erantha Welikala <eranthaw@atlinkcom.com>
2025-11-02 11:41:56 +0530

    Remove CLI code and keep only desktop version

--- mac/create-img.sh
+++
@@ -1,7 +0,0 @@
-create-dmg \
-  --volname "Git Profile Manager" \
-  --window-size 500 300 \
-  --icon-size 100 \
-  --app-drop-link 300 200 \
-  git-profile-manager-dmg/installer/GitProfileManager.dmg \
-  git-profile-manager-dmg/scripts
